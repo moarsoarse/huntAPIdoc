a8232a163b4ada81ae93d7286cb04034cb9ffb70
moarse <ivan.don4enko@gmail.com>
2023-11-27 13:53:25 +0300

    Правка имен

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNzUyOQ/spectral.js?branch_slug=main&token=87c589ae-08b2-4601-80b8-71910bfe4a87";
+export default { extends: ruleset };
